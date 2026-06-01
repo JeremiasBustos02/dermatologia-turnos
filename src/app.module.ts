f5353a97cb3d8633ec1809c6f11409c8df0fbbf3
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2026-06-01 05:00:11 -0300

    feat(professionals): implemente CRUD professionals con especialidad y cobertura

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SpecialtiesModule } from './modules/specialties/specialties.module';
 import { CoveragesModule } from './modules/coverages/coverages.module';
+import { ProfessionalsModule } from './modules/professionals/professionals.module';
 
 @Module({
   imports: [
     PrismaModule,
     SpecialtiesModule,
     CoveragesModule,
+    ProfessionalsModule,
   ],
 })
 export class AppModule {}
