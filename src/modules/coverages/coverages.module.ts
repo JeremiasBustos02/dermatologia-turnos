85e15b3eae3616da9a8ea893f073f9bd3d33e1f6
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2026-06-01 06:16:16 -0300

    chore: limpieza de archivos iniciales de github de la raiz

--- src/modules/coverages/coverages.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoveragesService } from './coverages.service';
-import { CoveragesController } from './coverages.controller';
-
-@Module({
-  controllers: [CoveragesController],
-  providers: [CoveragesService],
-})
-export class CoveragesModule {}
--- src/modules/coverages/dto/update-coverage.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCoverageDto } from './create-coverage.dto';
-
-export class UpdateCoverageDto extends PartialType(CreateCoverageDto) {}
