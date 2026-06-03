1404c220aaf27dd78aafef3902334b92e125af75
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2026-06-03 12:42:18 -0300

    feat: add auth/me endpoint

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+
+import type { Request } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth-guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
-  ) {}
+  ) { }
 
   @Post('login')
   login(@Body() dto: LoginDto) {
@@ -14,5 +24,17 @@ export class AuthController {
       dto.dni,
       dto.password,
     );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  me(@Req() req: Request) {
+    const user = req.user as any;
+
+    return {
+      id: user.userId,
+      dni: user.dni,
+      role: user.role,
+    };
   }
 }
